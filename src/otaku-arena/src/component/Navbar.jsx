8153c4d8ab133b6daf1cdb9c0463fd4d5e68f5aa
Tyron <tyronodame5@gmail.com>
2025-03-18 14:36:29 -0400

    created component folder with Navbar component

---
+++ src/otaku-arena/src/component/Navbar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-title">
+          <h1>OTAKU ARENA</h1>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
