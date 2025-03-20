fb8533c71b30ae01933cc2a13f58393f000d7d63
Tyron <tyronodame5@gmail.com>
2025-03-20 18:21:27 -0400

    fixed the fetches

--- src/otaku-arena/src/component/Navbar.jsx
+++ src/otaku-arena/src/component/Navbar.jsx
@@ -1,11 +1,14 @@
 import noodle from '../../public/noodle.jpeg';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <>
       <div className="navbar">
         <div className="brand-name">
-          <img src={noodle} alt="" id="logo" />
+          <Link to={'/'}>
+            <img src={noodle} alt="Logo" id="logo" />
+          </Link>
         </div>
         <div id="rules-button">
           <button>Rules</button>
