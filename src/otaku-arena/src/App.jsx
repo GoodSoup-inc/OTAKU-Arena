cc50fa1079fb7c98fe3d4ab17a4cf5ad8fea7988
Taylor <taybelle2020@gmail.com>
2025-03-20 12:37:03 -0400

    update

--- src/otaku-arena/src/App.jsx
+++ src/otaku-arena/src/App.jsx
@@ -3,15 +3,21 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import NavBar from "./component/Navbar";
-
+import CharBoxes from "./pageTwo/actionPage";
+// import
+/////////////////////////////////////////////
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
       <NavBar />
+      <br></br>
+      <br></br>
+      <br></br>
+      <CharBoxes></CharBoxes>
     </>
   );
 }
-
+//////////////////////////////////////////////////
 export default App;
