b332fe5992515261b6fd15c53bb638823ae26271
Taylor <taybelle2020@gmail.com>
2025-03-21 11:52:50 -0400

    draft

--- src/otaku-arena/src/App.jsx
+++ src/otaku-arena/src/App.jsx
@@ -1,7 +1,6 @@
-y;
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 import NavBar from "./component/Navbar";
 import CharBoxes from "./pageTwo/actionPage";
